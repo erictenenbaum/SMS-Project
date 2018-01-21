29852bd3370815c86012f789ca3126ec2e5a3671
erictenenbaum <erictenenbaum10@gmail.com>
2018-01-21 09:35:24 -0700

    replaced phone numbers by variables using process.env

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var schedule = require('node-schedule');
+// var schedule = require('node-schedule');
 var later = require("later");
 
 const accountSid = keys.apikeys.accountSid;
 const authToken = keys.apikeys.authToken;
 
+var twilioNumber = keys.apikeys.TWILIO_PHONE_NUMBER;
+var recipientNumber = keys.apikeys.recipientNumber;
 
 
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
 
-function sendText(randomText) {
+function sendText(recipient, tNumber, randomText) {
 
 	client.messages
   .create({
-    to: '+17145880860',
-    from: '+14807711460',
+    to: recipient,
+    from: tNumber,
     body: randomText
   })
   .then(message => console.log(message.sid));
-
+  	// console.log("success");
 }
 
 
@@ -29,29 +31,43 @@ function sendText(randomText) {
 
 var textOne = new schedule.RecurrenceRule();
 textOne.dayOfWeek = [0, new schedule.Range(1, 6)];
-textOne.hour = 22;
+textOne.hour = 11;
 textOne.minute =04;
 
 
 var a = schedule.scheduleJob(textOne, function(){
   console.log('Today is recognized by Rebecca Black!');
-  sendText("Hey how are you");
+  sendText(recipientNumber, twilioNumber, "Hey how are you");
 });
 
 
 var textTwo = new schedule.RecurrenceRule();
 textTwo.dayOfWeek = [0, new schedule.Range(1, 6)];
-textTwo.hour = 22;
-textTwo.minute =05;
+textTwo.hour = 13;
+textTwo.minute =35;
 
 
 var b = schedule.scheduleJob(textTwo, function(){
   console.log('Today is recognized by Rebecca Black!');
-  sendText("I'm Ok");
+  sendText(recipientNumber, twilioNumber, "I'm Ok");
 });
 
 
-sendText("Hey you");
+var textThree = new schedule.RecurrenceRule();
+textThree.dayOfWeek = [0, new schedule.Range(1, 6)];
+textThree.hour = 16;
+textThree.minute =05;
+
+
+var c = schedule.scheduleJob(textThree, function(){
+  console.log('Today is recognized by Rebecca Black!');
+  sendText(recipientNumber, twilioNumber,"I'm Ok");
+});
+
+
+// sendText(recipientNumber, twilioNumber, "Please tell me this finally works");
+
+// console.log(typeof twilioNumber)
 
 
 
--- keys.js
+++ keys.js
@@ -1,5 +1,6 @@
 exports.apikeys = {
 	accountSid: process.env.accountSID,
 	authToken: process.env.authToken,
-	TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER
+	TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
+	recipientNumber: process.env.recipientNumber
 };
